cae16e15eef0572ac1674110a26497f006492935
Miguel Angel Bermeo <bermeom@javeriana.edu.co>
2017-11-08 18:31:47 -0500

    The holes already and put the torus in position

--- Short Project Kickoff/lab1.m
+++ Short Project Kickoff/lab1.m
@@ -35,65 +35,85 @@ figure();
 scatter3(coor_circle(1,:),coor_circle(2,:),coor_circle(3,:),'r','LineWidth',2);
 
 %% Help 3. What about a spiral in a cilinder
- clear all; close all;clc; 
+clear all; close all;clc; 
 d=0.3;
-n=9;
+n=8;
 m=8*n;
 r = 0.95;
 r1 = 0.95-d/2;
-r2 = d/2;
+r2 = d/2+0.001;
+Ptos0 =[];
 for i=0:(m)
-    Ptos(:,:,i+1)=troty((-pi*i)/m)*transl(r1,0,0)*trotz((2*pi*i)/n)*transl(r2,0,0);
+    Ptos0(:,:,i+1)=troty((-pi*i)/m)*transl(r1,0,0)*trotz((2*pi*i)/n)*transl(r2,0,0);
 end
-coor_circle=transl(Ptos)';
+spiral=transl(Ptos0)';
 figure;
 
-plot3(coor_circle(1,:),coor_circle(2,:),coor_circle(3,:),'g','LineWidth',2)
+plot3(spiral(1,:),spiral(2,:),spiral(3,:),'g','LineWidth',2)
 hold on
-scatter3(coor_circle(1,:),coor_circle(2,:),coor_circle(3,:),'LineWidth',2);
+scatter3(spiral(1,:),spiral(2,:),spiral(3,:),'LineWidth',2);
 % view (25,50)
 xlabel('x');
 ylabel('y');
 zlabel('z');
 grid on;
 axis equal
-max(coor_circle(3,:))
-load('Data_groove_weld_fv_torus.mat') % This is some data you can use
-plot3(Weld_points(1,:),Weld_points(2,:),Weld_points(3,:),'r','LineWidth',2)
-hold on
-scatter3(Weld_points(1,:),Weld_points(2,:),Weld_points(3,:),'r','LineWidth',2)
+
+% load('Data_groove_weld_fv_torus.mat') % This is some data you can use
+% plot3(Weld_points(1,:),Weld_points(2,:),Weld_points(3,:),'r','LineWidth',2)
+% hold on
+% scatter3(Weld_points(1,:),Weld_points(2,:),Weld_points(3,:),'r','LineWidth',2)
+% xlabel('x');
+% ylabel('y');
+% zlabel('z');
+% axis 'equal'
+
+m1 = 8;
+Ptos1 = [];
+for i=0:(m1-1)
+    Ptos1(:,:,i*2+1)=troty((-pi*(i+1)+pi/2)/m1)*transl(r1,0,0)*trotz(-pi/2)*transl(r2,0,0);
+    Ptos1(:,:,i*2+2)=troty((-pi*(i+1)+pi/2)/m1)*transl(r1,0,0)*trotz(pi/2)*transl(r2,0,0);
+end
+holes1=transl(Ptos1)';
+
+% figure();
+scatter3(holes1(1,:),holes1(2,:),holes1(3,:),'r','LineWidth',2);
 xlabel('x');
 ylabel('y');
 zlabel('z');
-axis 'equal'
+grid on;
+axis equal
+% view (25,50)
 
-max(Weld_points(3,:))
+m2 = 8;
+n2 = 4;
+Ptos=[];
+for i=1:(m2)
+        for (j=1:n2)
+            Ptos(:,:,(i-1)*n2+j)=troty((-pi*(i))/(m2)+pi/24+(pi*j)/100)*transl(r,0,0);%*trotz(2*pi)*transl(r2,0,0);
+        end
+end
+holes2=transl(Ptos)';
 
-%% Reading '.stl' file
-fv=stlread('Torus.stl')% fv is a struct with faces and vertices
-% figure(4)
-% patch(fv,'FaceColor',       [0.8 0.8 1.0], ...
-%          'EdgeColor',       'none',        ...
-%          'FaceLighting',    'gouraud',     ...
-%          'AmbientStrength', 0.15);
-% % Add a camera light, and tone down the specular highlighting
-% camlight('headlight');
-% material('dull');
-% % Fix the axes scaling, and set a nice view angle
-% axis('image');
-% axis 'equal'
-% view([-135 35]);
+% figure();
+scatter3(holes2(1,:),holes2(2,:),holes2(3,:),'m','LineWidth',2);
+xlabel('x');
+ylabel('y');
+zlabel('z');
+grid on;
+axis equal
+
+fv=stlread('Torus.stl');% fv is a struct with faces and vertices
 fh = [fv.vertices'; ones(1,size(fv.vertices,1))];
 fh=troty(pi)*fh;
 fh(1:3,:) = (r).*(fh(1:3,:)./(max(max(fh(1:3,:)))));
 fh = transl(r1,0,0)*fh;
 fv.vertices = fh(1:3,:)';
-
 % figure();
-patch(fv,'FaceColor',       [0.8 0.8 1.0], ...
+patch(fv,'FaceColor',       [0.7 0.7 1.0], ...
          'EdgeColor',       'none',        ...
          'FaceLighting',    'gouraud',     ...
-         'AmbientStrength', 0.15);
+         'AmbientStrength', 0.08);
 % Add a camera light, and tone down the specular highlighting
 camlight('headlight');
 material('dull');
@@ -101,25 +121,63 @@ hold on;
 grid on;
 % Fix the axes scaling, and set a nice view angle
 axis('image');
-axis 'equal'
+axis 'equal';
+% alpha 0.7;
 view([-135 35]);
+
+
+%%
+figure
+xlabel('x');
+ylabel('y');
+zlabel('z');
+axis 'equal';
+table = [1 3 3 1;0 0 5 5;4 4 6 6] 
+fill3(table(1,:),table(2,:),table(3,:),'r');
+alpha 0.3;
+
+norm(sum(table(1,:))/3)
+theta = pi*(30/180);%% 30
+fv=stlread('Torus.stl');% fv is a struct with faces and vertices
+fh = [fv.vertices'; ones(1,size(fv.vertices,1))];
+fh=troty(pi)*fh;
+fh(1:3,:) = (r).*(fh(1:3,:)./(max(max(fh(1:3,:)))));
+fh = transl(r/2+sum(table(1,:)-min(table(1,:)))/3+min(table(1,:)),sum(table(2,:)-min(table(2,:)))/3+min(table(2,:)),sum(table(3,:)-min(table(3,:)))/3+min(table(3,:)))*trotx(theta)*fh;
+fv.vertices = fh(1:3,:)';
+% figure();
+patch(fv,'FaceColor',       [0.7 0.7 1.0], ...
+         'EdgeColor',       'none',        ...
+         'FaceLighting',    'gouraud',     ...
+         'AmbientStrength', 0.08);
+% Add a camera light, and tone down the specular highlighting
+camlight('headlight');
+material('dull');
+hold on;
+grid on;
+% Fix the axes scaling, and set a nice view angle
+axis('image');
+axis 'equal';
+% alpha 0.7;
+view([-135 35]);
+
+
+
+
 
 %% Animating the Puma manipulator performing a task
 % *Initialize the workspace*:
 %%
 % We clear the workspace and set the value of some constants of the problem
-clc;
-clear;
-close all;
+% clc;clear;close all;
 radius=0.20;
 n=200;
 INI = transl(-0.25, 0.25,-0.5); %center of the part
 % Plot a sketch of the environment of the robot
 mdl_puma560
-figure(6)
+% figure(6)
 p560.plot(qz);
 hold on;
-circle1 = circle([-0.25 0.25 -0.5], radius);
+circle1 = holes1;%%circle([-0.25 0.25 -0.5], radius);
 circle2 = circle([-0.25 0.25 -0.5], 0.5*radius);
 plot3(circle1(1,:), circle1(2,:), circle1(3,:),'g','LineWidth',1);
 patch(circle2(1,:), circle2(2,:), circle2(3,:),'r');
@@ -128,5 +186,7 @@ for i=1:n
     Laser_Pose(:,:,i)= INI*trotx(-pi/2)*troty(2*pi*i/n)*transl(0, 0, -radius);
 end
 
-Q= p560.ikine6s(Laser_Pose, 'run');
+Q= p560.ikine6s(INI*holes1, 'run');
 p560.plot(Q);
+
+
